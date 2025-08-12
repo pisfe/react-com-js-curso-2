3d1c8baf167a981caaaeeda28120599a2088b7bf
pisfe <pedroicaro@outlook.com>
2025-08-11 21:26:46 -0300

    aula 01-01 trocando cores

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,23 @@
-import './colaborador.css'
+import './colaborador.css';
+import { AiFillCloseCircle } from 'react-icons/ai';
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+    return (
+        <div className="colaborador">
+            <AiFillCloseCircle
+                className='deletar' 
+                onClick={aoDeletar}
+                size={25}
+            />
+            <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+                <img src={colaborador.imagem} alt={colaborador.nome} />
+            </div>
+            <div className="rodape">
+                <h4>{colaborador.nome}</h4>
+                <h5>{colaborador.cargo}</h5>
+            </div>
         </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
+    );
 }
 
-export default Colaborador
+export default Colaborador;
